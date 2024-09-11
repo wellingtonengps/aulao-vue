7c40d238ab35b30dc24922acd671f8496d33d56b
Wellington Pereira <43380060+wellingtonengps@users.noreply.github.com>
2024-09-11 17:24:03 -0300

    props

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,16 @@ import PrimeiroComponente from "@/components/PrimeiroComponente.vue";
 import ComDados from "@/components/ComDados.vue";
 import ComDadosReativosPrimitivos from "@/components/ComDadosReativosPrimitivos.vue";
 import ComDadosReativosNaoPrimitivos from "@/components/ComDadosReativosNaoPrimitivos.vue";
+import EscutaEvento from "@/components/EscutaEvento.vue";
+import PropriedadesComputadas from "@/components/PropriedadesComputadas.vue";
+import PropriedadesAssistidas from "@/components/PropriedadesAssistidas.vue";
 </script>
 
 <template>
   <header>Vue</header>
+  <PropriedadesAssistidas/>
+  <PropriedadesComputadas/>
+  <EscutaEvento/>
   <ComDadosReativosNaoPrimitivos />
   <ComDadosReativosPrimitivos />
   <ComDados />
---
+++ src/components/EmiteEvento.vue
@@ -0,0 +1,29 @@
+<script setup>
+  import {defineProps, defineEmits } from "vue"
+
+  const props = defineProps({
+    item: String,
+    preco: Number
+  })
+
+  const emit = defineEmits(
+      [
+          "teste"
+      ])
+
+  function adicionar(){
+    emit("teste", props.preco)
+  }
+</script>
+
+<template>
+  <div class="componente">
+    <h1>Componete que emite Evento</h1>
+    <p>{{item}} (R${{preco.toFixed(2)}})</p>
+    <button @click="adicionar">Adicionar ao carrinho</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
